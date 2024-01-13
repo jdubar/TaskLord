0e2e0616c5f2657b6de6b188ca8ccb54dbf0f991
jdubar <55170139+jdubar@users.noreply.github.com>
2024-01-13 13:30:08 -0500

    Updated assembly

--- TaskLord/Properties/AssemblyInfo.cs
+++ TaskLord/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("Optum")]
 [assembly: AssemblyCopyright($"Copyright © Optum 2023")]
 [assembly: AssemblyProduct("TaskLord")]
 [assembly: AssemblyVersion("1.0.*")]
+[assembly: SupportedOSPlatform("windows")]
